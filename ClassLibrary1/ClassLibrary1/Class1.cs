024d94194a9c4223c3b7022f0d23c955a9ce2abc
maksimVAs73 <traffikingvraj@gmail.com>
2024-11-28 11:43:13 +0300

    Add files via upload

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        private string _chiefInspector = "Васильев Василий Иванович";
+        private string _departmentName = "Автоинспекция г. Чита";
+        private List<string> _employees = new List<string>() { "Иванов И.И.", "Зиронов Т.А.", "Миронов А.В.", "Васильев В.И." };
+
+        public string GetInspector() => _chiefInspector;
+
+        public string GetCarInspection() => _departmentName;
+
+        public bool SetInspector(string fullname)
+        {
+            if (_employees.Contains(fullname))
+            {
+                _chiefInspector = fullname;
+                return true;
+            }
+            return false;
+        }
+
+        public string GenerateNumber(int number, char symbol, int code)
+        {
+            return $"{char.ToUpper(symbol)}{number}_{code}";
+        }
+
+        public List<string> GetWorker() => _employees;
+
+        public bool AddWorker(string fullname)
+        {
+            if (!_employees.Contains(fullname))
+            {
+                _employees.Add(fullname);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
